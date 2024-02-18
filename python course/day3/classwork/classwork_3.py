1edf32ced801c558bbd4763368ee6f0ad315cb14
tsotneeee <tsotnebajelidze@gmail.com>
2024-02-18 17:15:13 +0400

    second commit

---
+++ python course/day3/classwork/classwork_3.py
@@ -0,0 +1,16 @@
+dad_age = "52"
+mom_age = "43"
+grandma_age = "67"
+dad_name = "რამინ ბაჯელიძე"
+mom_name = "მაკა მოსიაშვილი"
+grandma_name = "მარინა რატიანი"
+
+print("მამაჩემი არის " + dad_name + dad_age, "წლის, დედაჩემი: " + mom_name + mom_age, "წლის,და ბებია: " + grandma_name + grandma_age, "წლის" )
+
+
+
+
+
+
+
+
